e895a5d6a3598f3923573d16d5c351a1ba0b0828
akshay_singhi <akshay.athena@snapdeal.com>
2015-09-04 18:10:54 +0530

    Added Android SDK

---
+++ sd-cafe/mobile/SDcafe/platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/akshay/projects/snapdeal-wth/SDLunchBox/sd-cafe/mobile/SDcafe/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/home/akshay/projects/snapdeal-wth/SDLunchBox/sd-cafe/mobile/SDcafe/platforms/android/ant-build/CordovaApp.ap_ \
+/home/akshay/projects/snapdeal-wth/SDLunchBox/sd-cafe/mobile/SDcafe/platforms/android/ant-build/classes.dex \
+/home/akshay/projects/snapdeal-wth/SDLunchBox/sd-cafe/mobile/SDcafe/platforms/android/CordovaLib/ant-build/classes.jar \
