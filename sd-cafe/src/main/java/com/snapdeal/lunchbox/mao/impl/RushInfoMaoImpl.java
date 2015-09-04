a31a778974986f2deff4b5fcfbc3628e9604eb1f
himani <himani.jadoun@snapdeal.com>
2015-09-04 16:36:27 +0530

    rushinfo

---
+++ sd-cafe/src/main/java/com/snapdeal/lunchbox/mao/impl/RushInfoMaoImpl.java
@@ -0,0 +1,58 @@
+/**
+ *  Copyright 2015 Jasper Infotech (P) Limited . All Rights Reserved.
+ *  JASPER INFOTECH PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */  
+package com.snapdeal.lunchbox.mao.impl;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+import com.snapdeal.lunchbox.mao.RushInfoMao;
+import com.snapdeal.lunchbox.mongo.entity.Account;
+import com.snapdeal.lunchbox.mongo.entity.RushInfo;
+
+/**
+ *  
+ *  @version     1.0, 04-Sep-2015
+ *  @author himani
+ */
+@Repository
+public class RushInfoMaoImpl implements RushInfoMao{
+    
+    public static final AtomicLong idCounter = new AtomicLong(0);
+    @Autowired
+    private MongoOperations        mongoOperations;
+    
+    @Override
+    public void saveRushInfo(RushInfo rushInfo) {
+        rushInfo.setId(getId());
+        mongoOperations.save(rushInfo);
+    }
+
+    @Override
+    public List<RushInfo> getRusInfoList(){
+        
+        return null;
+        
+    }
+    private Long getId() {
+        if (idCounter.get() == 0) {
+            Query query = new Query();
+            query.with(new Sort(Sort.Direction.DESC, "_id"));
+            Object dbObj = mongoOperations.findOne(query, RushInfo.class);
+            if (dbObj != null && dbObj instanceof Account) {
+                Account dbAccount = (Account) dbObj;
+                idCounter.compareAndSet(0, (Long) dbAccount.getId());
+            } else {
+                idCounter.set(0);
+            }
+        }
+        return idCounter.incrementAndGet();
+    }
+}
