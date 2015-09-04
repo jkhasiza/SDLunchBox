c69e44d3fc1fa06cdb6f270087213ddfaaeb12f2
mukund.kumar <mukund.kumar@snapdeal.com>
2015-09-04 11:32:20 +0530

    Backend Project

---
+++ sd-cafe/src/main/java/com/snapdeal/lunchbox/helper/Constants.java
@@ -0,0 +1,17 @@
+/**
+ *  Copyright 2015 Jasper Infotech (P) Limited . All Rights Reserved.
+ *  JASPER INFOTECH PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */  
+package com.snapdeal.lunchbox.helper;
+
+/**
+ *  
+ *  @version     1.0, 04-Sep-2015
+ *  @author mukund
+ */
+public interface Constants {
+    public static final String SUCCESS   = "0";
+    public static final String FAIL      = "1";
+    public static final String EXCEPTION = "2";
+    public static final String OK        = "OK";
+}
