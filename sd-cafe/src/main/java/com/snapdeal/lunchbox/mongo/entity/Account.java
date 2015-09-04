0525992ea2639469a78719053e73882bb57c97e6
Jitesh Kumar <jitesh.kumar@snapdeal.com>
2015-09-04 12:52:54 +0530

    Second Commit

---
+++ sd-cafe/src/main/java/com/snapdeal/lunchbox/mongo/entity/Account.java
@@ -0,0 +1,52 @@
+/**
+ *  Copyright 2015 Jasper Infotech (P) Limited . All Rights Reserved.
+ *  JASPER INFOTECH PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.snapdeal.lunchbox.mongo.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * @version 1.0, Sep 4, 2015
+ * @author jitesh
+ */
+@Document
+public class Account {
+
+    private Long   id;
+    private String mobileNumber;
+    private Long    groupId;
+    private String DeviceId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getDeviceId() {
+        return DeviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        DeviceId = deviceId;
+    }
+}
